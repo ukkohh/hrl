1427c800e99b62fb72a61f6431842478e695d47a
ukko hansen-haug <n0hauk00@students.oamk.fi>
2021-01-20 10:52:09 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [Age, setAge] = useState(0);
+  const [High, setHigh] = useState(0);
+  const [Low, setLow] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const high = (220 - Age) * 0.85;
+    const low = (220 - Age) * 0.65;
+    setHigh(high);
+    setLow(low);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Age</label>
+          <input type='number' value={Age} onChange={e => setAge(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <output>{Low}-{High}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
 }
